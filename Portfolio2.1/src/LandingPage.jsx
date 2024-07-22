328b7127d8d8ef7c25f1911f38e12f9321a330f7
Daniel <daniel.kroger@stryda.gg>
2024-07-22 15:44:02 +0200

    :)

---
+++ Portfolio2.1/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1 className="page-title">Daniel Nevado Kröger</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ Portfolio2.1/src/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+
+function Projects() {
+  return (
+    <div className='projects-page'>
+      Projects
+    </div>
+  )
+}
+
+export default Projects
