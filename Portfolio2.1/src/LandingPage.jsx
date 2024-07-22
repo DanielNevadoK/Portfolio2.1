3170b4d796e35cbdfc21502a6c98acf9a2dccf80
Daniel <daniel.kroger@stryda.gg>
2024-07-22 15:44:19 +0200

    :)

--- Portfolio2.1/src/LandingPage.jsx
+++ Portfolio2.1/src/LandingPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 
-
 function LandingPage() {
   return (
     <div className="landing-page">
-      <h1 className="page-title">Daniel Nevado Kröger</h1>
+      <div className="title-container">
+        <h1 className="mini-title">Nevado</h1>
+        <h1 className="page-title">Daniel</h1>
+        <h1 className="mini-title">Kröger</h1>
+      </div>
+      <div className="landing-page-pic">
+        <img src="../public/Img/No_bg.png" alt="Daniel" />
+      </div>
     </div>
   );
 }
