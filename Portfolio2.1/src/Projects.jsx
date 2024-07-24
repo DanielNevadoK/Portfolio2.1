7b97956e29a8e43b1793def4c149019900b78152
Daniel <daniel.kroger@stryda.gg>
2024-07-24 23:42:34 +0200

    projects base layout done :)

--- Portfolio2.1/src/Projects.jsx
+++ Portfolio2.1/src/Projects.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 
 function Projects() {
   return (
-    <div id='projects-page'>
-      Projects
+    <div id="projects-page">
+      <div>
+        <h2>PROJECTS</h2>
+      </div>
+      <div>
+        <section id="project-section1">
+          <div className="small-project-card">
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </div>
+          <div className="medium-project-card">
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </div>
+        </section>
+        <section id="project-section2">
+          <div className="medium-project-card">
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </div>
+          <div className="small-project-card">
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
