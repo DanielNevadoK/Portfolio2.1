7e36e4ac9b42b03f5a892d2b5f81f7df33b9f95e
Daniel <daniel.kroger@stryda.gg>
2024-07-24 03:14:32 +0200

    Updated fonts, Added Header/Navbar

---
+++ Portfolio2.1/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <button className='head-btn1'>Projects</button>
+      <button className='head-btn2'>About me</button>
+    </div>
+  )
+}
+
+export default Header
--- Portfolio2.1/src/LandingPage.jsx
+++ Portfolio2.1/src/LandingPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function LandingPage() {
   return (
     <div className="landing-page">
+      <Header />
       <div className="title-container">
-        <h1 className="mini-title1">Nevado</h1>
-        <h1 className="page-title">Daniel</h1>
-        <h1 className="mini-title2">Kröger</h1>
+        <div className="title-wrapper">
+          <h1 className="mini-title1">Nevado</h1>
+          <h1 className="page-title">Daniel</h1>
+          <h1 className="mini-title2">Kröger</h1>
+        </div>
       </div>
       <div className="landing-page-pic">
         <img src="/Img/No_bg.png" alt="Daniel" />
