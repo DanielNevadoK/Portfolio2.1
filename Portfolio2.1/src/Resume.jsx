3b368e1afaedabb223e56cf8bf76a72f78c1011d
Daniel <daniel.kroger@stryda.gg>
2024-07-26 01:30:28 +0200

    Resume section started :)

--- Portfolio2.1/src/Resume.jsx
+++ Portfolio2.1/src/Resume.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 
 function Resume() {
   return (
-    <div id='resume-page'>
-      Resume
+    <div id="resume-page">
+      <div className="resume-top-wrapper">
+        <div className="resume-title">
+          <h2>Resume</h2>
+        </div>
+        <div className="work-container">
+          <div className="mini-work-title">
+            <h3>WORK</h3>
+            <img src="" alt="SVG LinkedIn" />
+          </div>
+          <div className="experience-container">
+            <div className="exp1">
+              <p>Frontend developer (Internship)</p>
+              <p>02/24 - 05/24</p>
+              <p>KPI Media</p>
+            </div>
+            <div className="exp2">
+              <p>Frontend developer (Internship)</p>
+              <p>12/23 - 03/24</p>
+              <p>Stryda AB</p>
+            </div>
+            <div className="exp3">
+              <p>IT Projectmanager / Scrum Master</p>
+              <p>04/23 - 10/23</p>
+              <p>MTR Nordic AB</p>
+            </div>
+            <div className="exp4">
+              <p>Traffick managment</p>
+              <p>06/19 - 18/24</p>
+              <p>MTR Nordic AB</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
