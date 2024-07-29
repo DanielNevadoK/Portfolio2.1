76b293700499b2ba0247400f5004b181e070646f
Daniel <daniel.kroger@stryda.gg>
2024-07-29 02:52:38 +0200

    Skills layuout done

--- Portfolio2.1/src/Resume.jsx
+++ Portfolio2.1/src/Resume.jsx
@@ -61,67 +61,90 @@ function Resume() {
           </div>
         </div>
         <div className="skills-container">
-          <div className="mini-work-title">
-            <h3>SOFT SKILLS</h3>
-            <a href="https://github.com/DanielNevadoK" target="_blank">
-              <BiLogoGithub size="29" />
-            </a>
+          <div className="skills-wrp1">
+            <div className="mini-work-title">
+              <h3>SOFT SKILLS</h3>
+              <a href="https://github.com/DanielNevadoK" target="_blank">
+                <BiLogoGithub size="29" />
+              </a>
+            </div>
+            <div className="soft-skills">
+              <div className="SS-number">
+                <p>01</p>
+                <p>02</p>
+                <p>03</p>
+                <p>04</p>
+                <p>05</p>
+              </div>
+              <div className="SS-title">
+                <p>JavaScript</p>
+                <p>TypeScript</p>
+                <p>React</p>
+                <p>HTML</p>
+                <p>CSS</p>
+              </div>
+              <div className="SS-number">
+                <p>06</p>
+                <p>07</p>
+                <p>08</p>
+                <p>09</p>
+              </div>
+              <div className="SS-title">
+                <p>UX/UI</p>
+                <p>Git/GitHub</p>
+                <p>Figma</p>
+                <p>PhotoShop</p>
+              </div>
+            </div>
           </div>
-          <div className="soft-skills">
-            <div className="SS-number">
-              <p>01</p>
-              <p>02</p>
-              <p>03</p>
-              <p>04</p>
-              <p>05</p>
+          <div className="skills-wrp-container">
+            <div className="skills-wrp2">
+              <div className="mini-work-title">
+                <h3>LANGUAGE SKILLS</h3>
+              </div>
+              <div className="soft-skills">
+                <div className="SS-number">
+                  <p>10</p>
+                  <p>11</p>
+                  <p>12</p>
+                  <p>13</p>
+                </div>
+                <div className="SS-title">
+                  <p>Swedish</p>
+                  <p>English</p>
+                  <p>Spanish</p>
+                  <p>French</p>
+                </div>
+                <div className="SS-title2">
+                  <p>Native</p>
+                  <p>Fluent</p>
+                  <p>Native</p>
+                  <p>Beginner</p>
+                </div>
+              </div>
             </div>
-            <div className="SS-title">
-              <p>JavaScript</p>
-              <p>TypeScript</p>
-              <p>React</p>
-              <p>HTML</p>
-              <p>CSS</p>
-            </div>
-            <div className="SS-number">
-              <p>06</p>
-              <p>07</p>
-              <p>08</p>
-              <p>09</p>
-            </div>
-            <div className="SS-title">
-              <p>UX/UI</p>
-              <p>Git/GitHub</p>
-              <p>Figma</p>
-              <p>PhotoShop</p>
-            </div>
-          </div>
-          <div className="mini-work-title">
-            <h3>LANGUAGE SKILLS</h3>
-          </div>
-          <div className="soft-skills">
-            <div className="SS-number">
-              <p>10</p>
-              <p>11</p>
-              <p>12</p>
-              <p>13</p>
-            </div>
-            <div className="SS-title">
-              <p>Swedish</p>
-              <p>English</p>
-              <p>Spanish</p>
-              <p>French</p>
-            </div>
-            <div className="SS-title2">
-              <p>Native</p>
-              <p>Fluent</p>
-              <p>Native</p>
-              <p>Beginner</p>
+            <div className="skills-wrp2">
+              <div className="mini-work-title">
+                <h3>LANGUAGE SKILLS</h3>
+              </div>
+              <div className="soft-skills">
+                <div className="SS-number">
+                  <p>14</p>
+                  <p>15</p>
+                  <p>16</p>
+                  <p>17</p>
+                </div>
+                <div className="SS-title">
+                  <p>Acting</p>
+                  <p>Brazilian Jiu-Jitsu</p>
+                  <p>Video Games</p>
+                  <p>Traveling</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="resume-title2">
-
-        </div>
+        <div className="resume-title2"></div>
       </section>
     </div>
   );
