7d29e87b42b0a9b5fc1089c6166d86fda0e6e9ba
Daniel <daniel.kroger@stryda.gg>
2024-08-19 00:20:52 +0200

    tablet adjustment LP :)

--- Portfolio2.1/src/Resume.jsx
+++ Portfolio2.1/src/Resume.jsx
@@ -11,55 +11,8 @@ function Resume() {
         <div className="resume-title">
           <h2>Resume</h2>
         </div>
-        {/* <div className="work-container">
-          <div className="mini-work-title">
-            <h3>WORK EXPERIENCE</h3>
-            <a
-              href="https://www.linkedin.com/in/danielnevadokroger/"
-              target="_blank"
-            >
-              <FaLinkedin size="29" />
-            </a>
-          </div>
-          <div className="experience-container">
-            <div className="work_title">
-              <p>Frontend developer (Internship)</p>
-              <p>Frontend developer (Internship)</p>
-              <p>IT Projectmanager / Scrum Master</p>
-              <p>Traffick managment</p>
-            </div>
-            <div className="work_date">
-              <p>02/24 - 05/24</p>
-              <p>12/23 - 03/24</p>
-              <p>04/23 - 10/23</p>
-              <p>06/19 - 18/24</p>
-            </div>
-            <div className="work_company">
-              <p>KPI Media</p>
-              <p>Stryda AB</p>
-              <p>MTR Nordic AB</p>
-              <p>MTR Nordic AB</p>
-            </div>
-          </div>
-        </div> */}
       </section>
       <section className="resume-bottom-wrapper">
-        {/* <div className="cv-container">
-          <div className="cv-card">
-            <div className="cv-text-and-icon-container">
-              <div className="cv-text">
-                <p>Daniel</p>
-                <p>Nevado Kröger</p>
-              </div>
-              <a href="#" download="CV Daniel Nevado Kröger">
-                <FaFileDownload size={29} />
-              </a>
-            </div>
-            <div className="cv-img-container">
-              <img src="Img/Daniel_Nevado_Kroger.jpg" alt="CV-Profile-IMG" />
-            </div>
-          </div>
-        </div> */}
         <div className="work-container">
           <div className="mini-work-title">
             <h3>WORK EXPERIENCE</h3>
