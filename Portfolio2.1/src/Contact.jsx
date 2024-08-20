d1032dfcdff438d2562ee497bea38e7ab6105f7a
Daniel <daniel.kroger@stryda.gg>
2024-08-20 18:39:12 +0200

    contact mob-view adjustments :)

--- Portfolio2.1/src/Contact.jsx
+++ Portfolio2.1/src/Contact.jsx
@@ -11,7 +11,9 @@ function Contact() {
           <address className="email-container">
             <a href="mailto: daniel.nkroger@outlook.com">
               <p>Email</p>
-              <FaArrowRightLong />
+              <div className="arrow">
+                <FaArrowRightLong />
+              </div>
             </a>
           </address>
           <address className="LinkedIn-container">
@@ -20,7 +22,9 @@ function Contact() {
               target="_blank"
             >
               <p>LinkedIn</p>
-              <FaArrowRightLong />
+              <div className="arrow">
+                <FaArrowRightLong />
+              </div>
             </a>
           </address>
           <address className="discord-container">
@@ -29,21 +33,27 @@ function Contact() {
               target="_blank"
             >
               <p>Discord</p>
-              <FaArrowRightLong />
+              <div className="arrow">
+                <FaArrowRightLong />
+              </div>
             </a>
           </address>
         </div>
         <div className="contact-title">
           <h2>CONTACT</h2>
         </div>
-        <div className="back-to-top">
-          <a href="#landing-page">
-            <p>Back to top</p>
-            <FaArrowUp size={25} />
-          </a>
-        </div>
-        <div className="copy-right">
-          <p>Created and designed by Daniel Nevado Kröger</p>
+        <div className="btm-conatct-container">
+          <div className="back-to-top">
+            <a href="#landing-page">
+              <p>Back to top</p>
+              <div className="arrow">
+                <FaArrowUp size={25} />
+              </div>
+            </a>
+          </div>
+          <div className="copy-right">
+            <p>Created and designed by Daniel Nevado Kröger</p>
+          </div>
         </div>
       </div>
     </div>
