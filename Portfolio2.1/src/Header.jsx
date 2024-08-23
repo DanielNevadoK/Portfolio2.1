b48be8af3e2340ca0ab360a1f8fcd4e6aecbc11d
Daniel <daniel.kroger@stryda.gg>
2024-08-23 15:23:55 +0200

    Started burger menu for phone vw

--- Portfolio2.1/src/Header.jsx
+++ Portfolio2.1/src/Header.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  };
   return (
-    <div className="header">
+    <nav className="header">
+      <ul className={`head-menu-items ${isOpen ? "is-open" : ""}`}>
       <button className="head-btn1">
         <a href="#projects-page">Projects</a>
       </button>
       <button className="head-btn2">
         <a href="#resume-page">About me</a>
       </button>
-    </div>
+      <button className="head-btn3">
+        <a href="#contact-page">Contact</a>
+      </button>
+      </ul>
+      <button id="burger-menu" onClick={toggleMenu}>Trigger</button>
+    </nav>
   );
 }
 
