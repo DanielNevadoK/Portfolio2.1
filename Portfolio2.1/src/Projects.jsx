abfbc8a1eeffd40ef47626878aeae16f244fdd57
Daniel <daniel.kroger@stryda.gg>
2024-08-26 18:56:08 +0200

    Uppdated projects with real projects  :)

--- Portfolio2.1/src/Projects.jsx
+++ Portfolio2.1/src/Projects.jsx
@@ -9,48 +9,48 @@ function Projects() {
       </div>
       <div className="project-wrapper">
         <section id="project-section1">
-          <div className="small-project-card">
-            <div className="small-img-container">
-              <a href="#">
-                <img src="./Img/bagel_street_cafe.png" alt="Bagel Street Cafe" />
+          <div className="medium-project-card">
+            <div className="medium-img-container">
+              <a href="https://brokenrecords.vercel.app/" target="_blank">
+                <img src="./Img/Broken_records_pic.png" alt="" />
               </a>
             </div>
             <div className="description-container">
-              <p className="project-title">Bagel Street Café</p>
-              <p className="project-work" >UX/UI</p>
+              <p className="project-card-title">Broken Records</p>
+              <p className="project-work"> Vinyl Records Webshop</p>
             </div>
           </div>
           <div className="medium-project-card">
             <div className="medium-img-container">
               <a href="#">
-                <img src="/Img/Peluceria.png" alt="" />
+                <img src="./Img/Bagel_street_house.png" alt="" />
               </a>
             </div>
             <div className="description-container">
-              <p className="project-card-title">Bagel Street Café</p>
-              <p className="project-work">UX/UI</p>
+              <p className="project-title">Bagel House Café</p>
+              <p className="project-work">Bagel Cafe website</p>
             </div>
           </div>
           <div className="medium-project-card">
             <div className="medium-img-container">
               <a href="#">
-                <img src="/Img/Peluceria.png" alt="" />
+                <img src="./Img/Peluceria.png" alt="" />
               </a>
             </div>
             <div className="description-container">
-              <p className="project-title">Bagel Street Café</p>
-              <p className="project-work">UX/UI</p>
+              <p className="project-title">Spanish Hair Saloon</p>
+              <p className="project-work">UX/UI Design Prototype</p>
             </div>
           </div>
-          <div className="small-project-card">
-            <div className="small-img-container">
-              <a href="#">
-                <img src="./Img/bagel_street_cafe.png" alt="" />
+          <div className="medium-project-card">
+            <div className="medium-img-container">
+              <a href="https://dnk-dev.vercel.app/" target="_blank">
+                <img src="./Img/First_Portfolio.png" alt="" />
               </a>
             </div>
             <div className="description-container">
-              <p className="project-title">Bagel Street Café</p>
-              <p className="project-work">UX/UI</p>
+              <p className="project-title">First Porfolio</p>
+              <p className="project-work">Visit my first portfolio</p>
             </div>
           </div>
         </section>
