b1545860336324fcde9ddcd7621983683b8cf67b
Daniel <daniel.kroger@stryda.gg>
2024-09-01 19:24:47 +0200

    Added the general structure for the projects cases

--- Portfolio2.1/src/Project cases/BagelHouseCafe.jsx
+++ Portfolio2.1/src/Project cases/BagelHouseCafe.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 function BagelHouseCafe() {
   return (
-    <div>
-      
+    <div id='bagel-house-cafe'>
+        <div>
+            <h1>Short intro text</h1>
+            <img src="" alt="The project" />
+        </div>
+        <div>
+           <div><p>My role</p></div>
+           <div><p>Team (Solo)</p></div>
+           <div>Duration 5years</div> 
+        </div>
+        <div>
+            <img src="" alt="more pic of website" />
+        </div>
+        <div>
+            <div><p>01 overview</p></div>
+            <div><p>02 Problem</p></div>
+            <div><p>03 Design process </p></div>
+        <div><img src="" alt="figma flow" /></div>
+        <div>04 Selution and Resoults</div>
+        </div>
+        <div><h5>Visit the live site (if it excist)</h5></div>
+        <div>Other projects</div>
     </div>
   )
 }
