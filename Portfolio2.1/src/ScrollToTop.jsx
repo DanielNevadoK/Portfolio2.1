e724dbcdd4c61c8cc182f559859c2e1622a6682c
Daniel <daniel.kroger@stryda.gg>
2024-09-05 22:16:58 +0200

    Fixed landingpage job title bug

--- Portfolio2.1/src/ScrollToTop.jsx
+++ Portfolio2.1/src/ScrollToTop.jsx
@@ -5,7 +5,6 @@ function ScrollToTop({ children }) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Scroll to the top of the page when the pathname changes
     window.scrollTo(0, 0);
   }, [pathname]);
 
