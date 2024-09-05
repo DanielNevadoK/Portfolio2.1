a74016f66fe375422a3b4f7f97e6911a69d478a8
Daniel <daniel.kroger@stryda.gg>
2024-09-05 22:07:57 +0200

    Fixed routes scroll issue

--- Portfolio2.1/src/App.jsx
+++ Portfolio2.1/src/App.jsx
@@ -1,40 +1,49 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import LandingPage from './LandingPage'
-import Projects from './Projects'
-import Resume from './Resume'
-import Contact from './Contact'
-import BagelHouseCafe from './project_cases/BagelHouse/BagelHouseCafe';
-import BrokenRecords from './project_cases/broken_records/BrokenRecords';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./LandingPage";
+import Projects from "./Projects";
+import Resume from "./Resume";
+import Contact from "./Contact";
+import BagelHouseCafe from "./project_cases/BagelHouse/BagelHouseCafe";
+import BrokenRecords from "./project_cases/broken_records/BrokenRecords";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-<Router>
-      <Routes>
-        {/* Route for the main one-page layout */}
-        <Route 
-          path="/" 
-          element={
-            <>
-              <LandingPage />
-              <Projects />
-              <Resume />
-              <Contact />
-            </>
-          } 
-        />
-        {/* Route for the BagelHouseCafe project page */}
-        {/* <Route path="/project_cases/hair-salon-es" element={<HairSalonEs />} /> */}
-        
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/project_cases/broken-records" element={<BrokenRecords />} />
-        <Route path="/project_cases/bagel-house-cafe" element={<BagelHouseCafe />} />
-      </Routes>
+    <Router>
+      <ScrollToTop>
+        <Routes>
+          {/* Route for the main one-page layout */}
+          <Route
+            path="/"
+            element={
+              <>
+                <LandingPage />
+                <Projects />
+                <Resume />
+                <Contact />
+              </>
+            }
+          />
+          {/* Route for the BagelHouseCafe project page */}
+          {/* <Route path="/project_cases/hair-salon-es" element={<HairSalonEs />} /> */}
+
+          <Route path="/Projects" element={<Projects />} />
+          <Route
+            path="/project_cases/broken-records"
+            element={<BrokenRecords />}
+          />
+          <Route
+            path="/project_cases/bagel-house-cafe"
+            element={<BagelHouseCafe />}
+          />
+        </Routes>
+      </ScrollToTop>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- Portfolio2.1/src/project_cases/BagelHouse/BagelHouseCafe.jsx
+++ Portfolio2.1/src/project_cases/BagelHouse/BagelHouseCafe.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./BagelHouseCafe.css";
 import Tooltip from "@mui/material/Tooltip";
 import ProjectsHeader from "../projects_header/ProjectsHeader";
