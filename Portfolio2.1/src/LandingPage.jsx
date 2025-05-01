0412be54c4988a792bbf0132d1b72f56943090b4
Daniel N.K <daniel.nkroger@gmail.com>
2025-05-01 23:19:34 +0200

    Touch up for BG done

--- Portfolio2.1/src/LandingPage.jsx
+++ Portfolio2.1/src/LandingPage.jsx
@@ -8,27 +8,34 @@ import LeftTextAnimation from "./components/text-animation/left-text-animation/m
 
 function LandingPage() {
   return (
-    <div 
-    id="landing-page" 
-    className="-mt-24 w-full h-[105vh] flex flex-col items-center overflow-hidden relative
-    bg-[linear-gradient(0deg,_rgba(18,51,66,1)_0%,_rgba(22,64,83,1)_26%,_rgba(10,23,30,1)_100%)]">
+    <div
+      id="landing-page"
+      className="-mt-24 w-full h-[105vh] flex flex-col items-center overflow-hidden relative
+    bg-[radial-gradient(circle,_#0B1922_0%,_#164053_20%,_#03131B_55%)]
+"
+    >
       <div className="title w-full p-6 mt-[25%]">
-        <h1 className="mini-title">Daniel Nevado Kröger</h1>
+        <h1 id="fugaz-one" className="mini-title flex align-center ">
+          Daniel Nevado Kröger</h1>
         <div className="btn-container flex gap-11">
           <ProjectsBtn />
           <AboutMe />
           <ContactBtn />
         </div>
       </div>
-      <div className="landing-page-pic z-10">
-        <img src="/Img/No_bg.png" alt="Daniel" />
+      <div className="landing-page-pic z-10 max-w-[1350px] overflow-hidden items-center absolute top-[68%] left-[45%] translate-x-[-50%] translate-y-[-50%] ">
+        <img
+          className="max-w-[130%] height-auto display-block"
+          src="/Img/No_bg.png"
+          alt="Daniel"
+        />
       </div>
       <div className="job-title-lp">
-      <RightTextAnimation />
-      <LeftTextAnimation />
+        <RightTextAnimation />
+        <LeftTextAnimation />
       </div>
       <div className="absolute inset-0 bg-gradient-to-t from-[rgba(22,64,80,1)] from-1% to-[rgba(22,64,80,0)] to-45% z-20 pointer-events-none" />
-</div>
+    </div>
   );
 }
 
